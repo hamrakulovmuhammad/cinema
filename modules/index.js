2bf92b3cf68d28ccdcf66b9f359c1de5b7143579
hamrakulovmuhammad <hamrakulovm010@gmail.com>
2023-08-21 19:09:32 +0500

    fixed dinamick pages

--- modules/index.js
+++ modules/index.js
@@ -11,9 +11,23 @@ fetch(`https://api.themoviedb.org/3/movie/${poster_id}`, {
   .then((res) => reloadA(res));
 
 function reloadA(arr) {
-  console.log(arr);
   let body = document.querySelector("body");
-  console.log(
-    (body.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${arr.backdrop_path})`)
-  );
+  let movie = document.querySelector(".movie");
+  let h1 = document.querySelector(".info h1");
+  let h2 = document.querySelector(".info h2");
+  let p = document.querySelector(".info p");
+  let button = document.querySelector(".display_flex button");
+
+  button.onmouseenter = () => {
+    button.style.opacity = "1"
+  }
+  button.onmouseleave = () => {
+    button.style.opacity = "0.4"
+  }
+  console.log(movie);
+  h1.innerHTML = arr.title;
+  h2.innerHTML = arr.title;
+  p.innerHTML = arr.overview;
+  movie.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${arr.poster_path})`;
+  body.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${arr.backdrop_path})`;
 }
